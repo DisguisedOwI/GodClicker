afd77e28511429f42d7eaa52ba3909310a100d00
DisguisedOwI <owl@disguised.name>
2025-10-11 19:02:40 +0200

    hold_offset_checked

--- Main.py
+++ Main.py
@@ -41,6 +41,7 @@ get_position_button_enabled = False  # Variable to track get position button sta
 ctk.set_appearance_mode("System")  # Modes: system (default), light, dark
 ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
+
 # Function to validate numeric input only
 def validate_numeric_input(char):
     """Allow only digits and decimal points"""
